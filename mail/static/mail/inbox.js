2f8ac7ea94614df337132d6a0452ed08e32b1398
GF <farnellg@gmail.com>
2023-01-07 12:34:53 -0500

    asdf

--- mail/static/mail/inbox.js
+++ mail/static/mail/inbox.js
@@ -23,11 +23,12 @@ function compose_email() {
 }
 
 function load_mailbox(mailbox) {
-  
+
   // Show the mailbox and hide other views
   document.querySelector('#emails-view').style.display = 'block';
   document.querySelector('#compose-view').style.display = 'none';
 
   // Show the mailbox name
-  document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
+  document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() +
+    mailbox.slice(1)}</h3>`;
 }
